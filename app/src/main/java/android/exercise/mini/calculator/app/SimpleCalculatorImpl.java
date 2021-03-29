07683808b787afc244d229305b18efb5b1b1a563
gadi <gadi.fleischer@mail.huji.ac.il>
2021-03-29 15:49:13 +0300

    fixed

--- app/src/main/java/android/exercise/mini/calculator/app/SimpleCalculatorImpl.java
+++ app/src/main/java/android/exercise/mini/calculator/app/SimpleCalculatorImpl.java
@@ -6,8 +6,6 @@ public class SimpleCalculatorImpl implements SimpleCalculator {
 
   private String outputStr = "";
   private int res= 0;
-  private String curNum = "";
-  private String lastAction = "";
   @Override
   public String output() {
     return this.outputStr;
@@ -16,55 +14,62 @@ public class SimpleCalculatorImpl implements SimpleCalculator {
   @Override
   public void insertDigit(int digit) {
     String digitStr = String.valueOf(digit);
-    this.curNum += digitStr;
     this.outputStr+=digitStr;
   }
 
   @Override
   public void insertPlus() {
-    if(!this.curNum.equals("")) {
+    if(!this.outputStr.equals("")) {
+      String lastChar = this.outputStr.substring(this.outputStr.length() - 1);
+      if(!lastChar.equals("+") && !lastChar.equals("-"))
       this.outputStr+="+";
-      calcRes();
-      this.lastAction="+";
     }
   }
 
   @Override
   public void insertMinus() {
-    if(!this.curNum.equals(""))
-      this.outputStr+="-";
-      calcRes();
-      this.lastAction="-";
+    if(!this.outputStr.equals("")) {
+      String lastChar = this.outputStr.substring(this.outputStr.length() - 1);
+      if(!lastChar.equals("+") && !lastChar.equals("-"))
+        this.outputStr+="-";
+    }
   }
 
   private void calcRes() {
-    if(this.lastAction.equals("")){
-      this.res = Integer.parseInt(this.curNum);
-    } else if (this.lastAction.equals("+")) {
-      this.res += Integer.parseInt(this.curNum);
-    } else {
-      this.res -= Integer.parseInt(this.curNum);
+    String[] parts = this.outputStr.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
+    double result = Double.parseDouble(parts[0]);
+    for (int i = 1; i < parts.length; i += 2) {
+      String op = parts[i];
+      double val = Double.parseDouble(parts[i+1]);
+      switch (op) {
+        case "+" :
+          result += val;
+          break;
+        case "-" :
+          result -= val;
+          break;
+      }
     }
-    this.curNum="";
+    this.res = (int)result;
   }
 
   @Override
   public void insertEquals() {
-    calcRes();
-    this.lastAction="";
-    this.outputStr=String.valueOf(this.res);
-    this.curNum= String.valueOf(this.res);
+    if(this.outputStr.length()==0) {
+      this.res = 0;
+      this.outputStr = String.valueOf(this.res);
+      return;
+    }
+    String lastChar = this.outputStr.substring(this.outputStr.length() - 1);
+    if(!lastChar.equals("+") && !lastChar.equals("-")){
+      calcRes();
+      this.outputStr=String.valueOf(this.res);
+    } //else print invalid operation
   }
 
   @Override
   public void deleteLast() {
     if(!this.outputStr.equals("")){
-      String lastChar = this.outputStr.substring(this.outputStr.length() - 1);
-      if(lastChar.equals("+") || lastChar.equals("-")){
-        this.lastAction="";
-      }else{
-        this.curNum=this.curNum.length()==0?"":this.curNum.substring(0, this.curNum.length() - 1);
-      }
       this.outputStr= this.outputStr.substring(0, this.outputStr.length() - 1);
     }
   }
@@ -73,14 +78,12 @@ public class SimpleCalculatorImpl implements SimpleCalculator {
   public void clear() {
     this.outputStr = "";
     this.res= 0;
-    this.curNum = "";
-    this.lastAction = "";
   }
 
   @Override
   public Serializable saveState() {
     CalculatorState state = new CalculatorState();
-    state.setState(this.outputStr,this.res,this.curNum,this.lastAction);
+    state.setState(this.outputStr,this.res);
     return state;
   }
 
@@ -91,31 +94,19 @@ public class SimpleCalculatorImpl implements SimpleCalculator {
     }
     CalculatorState casted = (CalculatorState) prevState;
     this.outputStr=casted.getOutputStr();
-    this.curNum=casted.getCurNum();
-    this.lastAction=casted.getLastAction();
     this.res=casted.getRes();
   }
 
   private static class CalculatorState implements Serializable {
     private String outputStr = "";
     private int res= 0;
-    private String curNum = "";
-    private String lastAction = "";
 
-    public void setState(String output, int res, String curNum, String lastAction){
+    public void setState(String output, int res){
       this.outputStr=output;
       this.res=res;
-      this.curNum=curNum;
-      this.lastAction=lastAction;
     }
     public String getOutputStr(){
       return this.outputStr;
-    }
-    public String getCurNum(){
-      return this.curNum;
-    }
-    public String getLastAction(){
-      return this.lastAction;
     }
     public int getRes(){
       return this.res;
